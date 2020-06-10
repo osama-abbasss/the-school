a0cbcc9bcde29f54d92d07a0013b365b131c2303
osama-abbasss <eng.oabbass@gmail.com>
2020-06-10 22:43:18 +0200

    sqlite3

--- src/project/settings.py
+++ src/project/settings.py
@@ -85,11 +85,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),}}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'), }}
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -97,9 +98,10 @@ DATABASES = {
         'USER': 'oza',
         'PASSWORD': 'osama811',
         'HOST': 'localhost',
-        'PORT': '',
+        'PORT': '5432',
     }
 }
+"""
 
 
 # Password validation
