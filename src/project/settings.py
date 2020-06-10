505ee2301a106f946be61e6a47da6e7bbc7d1dbd
osama-abbasss <eng.oabbass@gmail.com>
2020-06-10 21:12:19 +0200

    use postgresql

--- src/project/settings.py
+++ src/project/settings.py
@@ -85,10 +85,19 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),}}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'school',
+        'USER': 'oza',
+        'PASSWORD': 'osama811',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
